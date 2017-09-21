4eead67ce1704514b1c2a38df0e94d6b2580bbf8
Park HyeongJin <jt3205@users.noreply.github.com>
2017-09-21 17:28:44 +0900

    2017-09-21

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ var path = require('path');
 var app = express();
 var port = 3000;
 
+var credentials = require('./secret.js');
+
 app.use(require('body-parser').urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+app.use(require('express-session')({
+	resave:false,
+	saveUninitialized:false,
+	secret:credentials.cookieSecret
+}));
+
+app.use(function (req, res, next) {
+	if(req.session.flashMsg != undefined){
+		res.locals.flash = req.session.flashMsg;
+		delete req.session.flashMsg;
+	}
+	next();
+});
 
 var mysql = require('mysql');
 
@@ -23,14 +41,20 @@ var client = mysql.createConnection({
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/page', function(req, res){
+	var isLogin = req.session.user != undefined ? true : false;
+	var level;
+	if(isLogin){
+		level = req.session.user.level;
+	} else {
+		level = 0;
+	}
 	client.connect(function (err) {
 		client.query('SELECT * FROM user', function(error, result, fields){
 			if(error){
 				console.log('쿼리에 오류가 발생했습니다');
 				res.render('index', {flag:false});
 			}else {
-				console.log(result);
-				res.render('index', {flag:true, data:result});
+				res.render('index', {flag:true, data:result, login:isLogin, level:level});
 			}
 		});
 	});
@@ -40,28 +64,23 @@ app.get('/post',function (req, res) {
 	res.render('post');
 });
 
-app.post('/process-post', function(req, res){
-	console.log(req.body);
-});
-
 app.post('/process-post', function (req, res) {
-	var sql = "insert into 'user'('id','password','name','email') values('"+
-			req.body.uid + "', password('" +
-			req.body.upw + "'), '" + 
-			req.body.uname + "', '" + 
-			req.body.uemail + "')";
+	var sql = "INSERT INTO `user`(`id`, `password`, `name`, `email`) VALUES('"+
+			req.body.id + "', '" +
+			req.body.pw + "', '" + 
+			req.body.name + "', '" + 
+			req.body.email + "');";
 	client.query(sql, function (err, result) {
 		if(err){
 			res.render('result', {msg:err.code});
-		}else {
+		} else {
 			res.render('result', {msg:'성공적으로 입력되었습니다.'})
 		}
 	});
 });
 
 app.get('/del-process', function (req, res) {
-	console.log(req.query.id);
-	var sql = "Delete from 'user' where 'id' = '"+req.query.id+"'";
+	var sql = "Delete from user where id='"+req.query.id+"';";
 	client.query(sql, function (err) {
 		if(err){
 			res.render('result', {msg:error.code});
@@ -69,6 +88,36 @@ app.get('/del-process', function (req, res) {
 			res.redirect(303, '/page');
 		}
 	});
+});
+
+app.post('/process-login', function (req, res) {
+	var sql = "Select * from user where id='"+req.body.id+
+			  "' and password='"+req.body.pw+"';";
+	client.query(sql, function (err, result) {
+		if(err){
+			res.render('result', {msg:err.code});
+		} else {
+			if(result.length == 1){
+				req.session.user = result[0];
+				res.redirect(303, '/page');
+			} else {
+				res.render('result', {msg:'아이디나 비밀번호가 올바르지 않습니다.'})
+			}
+		}
+	});
+});
+
+app.get('/login', function(req, res){
+	res.render('login');
+});
+
+app.get('/log-out', function (req, res) {
+	delete req.session.user;
+	req.session.flashMsg = {
+		type:'success',
+		msg:'로그아웃 되었습니다'
+	}
+	res.redirect(303, '/page');
 });
 
 app.listen(port, function () {
